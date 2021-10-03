85e6e803222d7244525d3e15d17c9b6d084328b7
Filip Bjurstrøm <filip18@live.no>
2021-10-03 19:24:46 +0200

    Trying out writing computed features to textfile in order to save them

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import radiomics
+import logging
 from patient_classes import Patient, StudyGroup
 from radiomics import featureextractor, getTestCase, firstorder
 import SimpleITK as sitk
+from main import remove_disqualified_patients
 
 
 def initiate_featureextractor(settings=dict):
@@ -23,8 +26,8 @@ def calculate_firstorder_features(
     masks = sitk.GetImageFromArray(patient.return_GTV_segmentations(filepath))
 
     extractor = initiate_featureextractor()
-
-    extractor.enableFeatureClassByName(featureClass="firstorder")
+    extractor.enableFeaturesByName(firstorder=["Energy"])
+    #extractor.enableFeatureClassByName(featureClass="firstorder")
 
     print(f"\nCalculating first-order features for patient {patient}")
     # The feature vector returned by execute is a collections.OrderedDict
@@ -34,6 +37,7 @@ def calculate_firstorder_features(
             print(f"Computed {featurename}: {feature_dict[featurename]}")
 
     patient.firstorder_features = feature_dict
+    return feature_dict
 
 
 if __name__ == '__main__':
@@ -41,12 +45,27 @@ if __name__ == '__main__':
     lung1_path = "C:/Users/filip/Desktop/image-data/manifest-Lung1/NSCLC-Radiomics"
     # 014, 021, 085 and 194 are excluded due errors in the files provided for these patients, 128 is excluded
     # due to no segmentatiion file being provded at all (post-operative case, acounted for in study)
-    disq_patients = ["LUNG1-014", "LUNG1-021", "LUNG1-085", "LUNG1-194", "LUNG1-128"]
+    disq_patients = ["LUNG1-014", "LUNG1-021", "LUNG1-085", "LUNG1-194", "LUNG1-128", "LUNG1-095"]
 
     # Initiating our studygroup, adding all patients, and removing those that are excluded
     lung1 = StudyGroup()
     lung1.add_all_patients(csv_path)
-    patient1 = lung1.patients[0]
+    remove_disqualified_patients(lung1, disq_patients)
 
-    calculate_firstorder_features(patient1, lung1_path)
-    print(patient1.firstorder_features)
+    patient1: Patient = lung1.patients[91]
+    print(patient1)
+    print(np.shape(patient1.return_image_array(lung1_path)), np.shape(patient1.return_GTV_segmentations(lung1_path)))
+    #patient1.view_segmentations(lung1_path)
+
+
+    f = open("firstorder.data", "w")
+    for patient in lung1:
+        feature = calculate_firstorder_features(patient, lung1_path, mute=False)
+        f.write(str(feature["original_firstorder_Energy"]) + "\n")
+        print(f"{patient.patientID}: Energy = {patient.firstorder_features}")
+    f.close()
+
+
+    #calculate_firstorder_features(patient1, lung1_path)
+
+
