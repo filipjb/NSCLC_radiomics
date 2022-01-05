ecc821cd6afae06ae8b35540d883fa065bc499fa
Filip Bjurstrøm <filip18@live.no>
2022-01-05 15:01:31 +0100

    Got haukeland extraction working in test.py, need to adjust some mask positions, and maybe make function more efficient

--- test.py
+++ test.py
@@ -5,10 +5,8 @@ import glob
 import numpy as np
 from skimage.draw import polygon
 import matplotlib.pyplot as plt
-import pandas as pd
 import kaplanmeier as km
 from patient_classes import Patient, StudyGroup, slice_viewer
-import seaborn as sns
 
 testpath = r"C:\Users\filip\Desktop\haukeland_test\RS.1.2.246.352.205.4628746736953205655.4330711959846355332.dcm"
 
@@ -57,7 +55,7 @@ def get_contour_coords(path):
 def get_haukeland_data(path, structure="GTV"):
     os.chdir(path)
     ct_dict = dict()
-    contours = dict()
+    masks = dict()
 
     ct_filelist = glob.glob(os.path.join(os.getcwd(), r"CT*.dcm"))
     rs_filename = glob.glob(os.path.join(os.getcwd(), r"RS*.dcm"))
@@ -78,55 +76,38 @@ def get_haukeland_data(path, structure="GTV"):
         patient_z = ct.ImagePositionPatient[2]
         ps = ct.PixelSpacing[0]
 
-        seq = dicom.dcmread(rs_filename[0])
-        # Finding the contournumber of the structure we are segmenting
-        for i in range(len(seq.StructureSetROISequence)):
-            if re.search(structure, seq.StructureSetROISequence[i].ROIName):
-                contourNumber = i
+        seq = dicom.dcmread(rs_filename[0])     # The dicomfile for the segmented structures
+        # Finding the contournumber of the selected structure, such that we can extract it from ROIContourSequence
+        structureNames = [seq.StructureSetROISequence[i].ROIName for i in range(len(seq.StructureSetROISequence))]
+        contourNumber = [i for i, item in enumerate(structureNames) if re.search(structure, item)][0]
 
-        ds = seq.ROIContourSequence[contourNumber].ContourSequence
-        print(ds)
-
-        ct_sop = ct.SOPInstanceUID
-
-        seg_sop_list = [ds[k].ContourImageSequence[0].ReferencedSOPInstanceUID for k in range(len(ds))]
-        print(ct_sop in seg_sop_list)
-
-        quit()
-
+        # The contoursequence of the structure we have chosen
         ds = seq.ROIContourSequence[contourNumber].ContourSequence
 
-        # In this Sequence, the contour coordiantes array of each entry is saved as a 1d array
-        # in the tag ContourData, so we reshape the array when we retrieve it
-        for n in ds:
-            contourList = np.array(n.ContourData)
-            # Each contoured pointed is stord sequentially; x1, y1, z1, x2, y2, z2, ..., so the array is reshaped
-            # thus the contour variable contains the coordinates of the contour line around the structure
-            contour = np.reshape(contourList, (len(contourList) // 3, 3))
-            contours.update({patient_z: contour})
+        totalMask = np.zeros([ct.pixel_array.shape[0], ct.pixel_array.shape[1]])
+        for element in ds:
+            # If the UID of the contour matches the UID of the sequence, we retrieve the contour:
+            if element.ContourImageSequence[0].ReferencedSOPInstanceUID == ct.SOPInstanceUID:
+                contour = np.array(element.ContourData)
+                # Each contoured pointed is stored sequentially; x1, y1, z1, x2, y2, z2, ..., so the array is reshaped
+                # thus the contour variable contains the coordinates of the contour line around the structure
+                contour = np.reshape(contour, (len(contour) // 3, 3))
+                print(contour)
+                # Make the contour into a mask:
+                contourMask = np.zeros([512, 512])
+                r, c = polygon(contour[:, 0] - patient_x, contour[:, 1] - patient_y, contourMask.shape)
+                contourMask[r, c] = 1
+                totalMask += np.rot90(contourMask)
+
+        masks.update({patient_z: totalMask > 0})
 
     # Sorting the ct dict by image slice position:
     sorted_dict = {k: v for k, v in sorted(ct_dict.items(), key=lambda item: -item[0])}
     ct_images = np.array(list(sorted_dict.values()))
 
     # Sorting patient contours by slice position:
-    sorted_contours = {k: v for k, v in sorted(contours.items(), key=lambda item: -item[0])}
-    contour_arrays = np.array(list(sorted_contours.values()))
-
-    # -------- Contour to mask conversion -------- #
-    # A list binary image masks that will be returned to the user
-    masks = []
-    # Going through each contour
-    for cont in contour_arrays:
-        # Creating a black image
-        mask = np.zeros([512, 512])
-        # Drawing a polygon at the coordinates of the contour and setting the polygon coordinates
-        # in the black image to 1, as well as adjusting for patient position
-        r, c = polygon(cont[:, 0] - patient_x, cont[:, 1] - patient_y, mask.shape)
-        mask[r, c] = 1
-        masks.append(mask)
-
-    ct_masks = np.array(masks)
+    sorted_contours = {k: v for k, v in sorted(masks.items(), key=lambda item: -item[0])}
+    ct_masks = np.array(list(sorted_contours.values()))
 
     return ct_images, ct_masks
 
@@ -140,9 +121,9 @@ if __name__ == '__main__':
     Lung1_group = StudyGroup()
     #Lung1_group.add_all_patients(csv_path)
 
-    ims, masks = get_haukeland_data(hauk_path)
+    ims, masks = get_haukeland_data(hauk_path, "Lungs")
 
-    slice_viewer(masks)
+    slice_viewer(np.multiply(masks, ims))
     '''
     stats_df = pd.read_csv(csv_path)
     firstorder_df = pd.read_csv("pythondata/feature_files/firstorder.csv")
