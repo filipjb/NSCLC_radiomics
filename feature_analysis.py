29e8eee3bc048e8bad172c51168de512ac834ec0
Filip Bjurstrøm <filip18@live.no>
2022-03-07 20:48:07 +0100

    made plot_km for creating of kapplan meier curves

--- feature_analysis.py
+++ feature_analysis.py
@@ -1,41 +1,56 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import kaplanmeier as km
-
-huh_shape = pd.read_csv(r"feature_files\HUH_shape.csv")
-lung1_shape = pd.read_csv(r"feature_files\lung1_shape.csv")
-lung1_clinical = pd.read_csv(
-    r"C:\Users\filip\Desktop\radiomics_data\NSCLC Radiomics Lung1.clinical-version3-Oct 2019.csv"
-)
-disq_patients = ["LUNG1-014", "LUNG1-021", "LUNG1-085", "LUNG1-095", "LUNG1-194", "LUNG1-128"]
-
-# Removing the patients in Lung1 that were disqualified
-for i in disq_patients:
-    lung1_clinical = lung1_clinical.drop(lung1_clinical[lung1_clinical.PatientID == i].index[0])
-
-df = pd.merge(lung1_shape, lung1_clinical, on='PatientID', how='outer')
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import logrank_test
 
 
-def plot_km(dataframe, parameter, threshtype="median"):
+def plot_km(dataframe, parameter: str, groupname: str, xlim=1500):
 
+    group1 = dataframe[dataframe[parameter] > dataframe[parameter].median()]
+    group2 = dataframe[dataframe[parameter] <= dataframe[parameter].median()]
 
+    t1 = group1["Survival.time"]
+    t2 = group2["Survival.time"]
+    e1 = group1["deadstatus.event"]
+    e2 = group2["deadstatus.event"]
 
-    df1 = df[df["deadstatus.event"] == 1]
-    df2 = df[df["deadstatus.event"] == 0]
+    kmf = KaplanMeierFitter()
 
-    time = df["Survival.time"]
-    censoring = df["deadstatus.event"]
-    labx = df["Compactness2"] > df["Compactness2"].median()
+    kmf.fit(t1, e1, label=f"{parameter} > median")
 
-    out = km.fit(time, censoring, labx)
-    km.plot(out)
+    ax = kmf.plot_survival_function(ci_show=False, color="blue")
+    ax.set_xlim(0, xlim)
+
+    kmf.fit(t2, e2, label=f"{parameter} <= median")
+    kmf.plot_survival_function(ax=ax, ci_show=False, color="red")
+
+    lr_result = logrank_test(t1, t2, e1, e2)
+    pval = lr_result.p_value
+
+    plt.title(f"{groupname} {parameter}, Logrank P-value = {pval.__round__(5)}")
+    plt.ylabel("Survival probability")
+    plt.xlabel("Survival time (days)")
+
     plt.show()
 
 
+if __name__ == '__main__':
 
+    lung1_shape = pd.read_csv(r"feature_files\lung1_shape.csv")
+    lung1_firstorder = pd.read_csv(r"feature_files\lung1_firstorder.csv")
+    lung1_glrlm = pd.read_csv(r"feature_files\lung1_GLRLM.csv")
+    lung1_hlh = pd.read_csv(r"feature_files\lung1_HLH_GLRLM.csv")
 
+    lung1_clinical = pd.read_csv(
+        r"C:\Users\filip\Desktop\radiomics_data\NSCLC Radiomics Lung1.clinical-version3-Oct 2019.csv"
+    )
+    disq_patients = ["LUNG1-014", "LUNG1-021", "LUNG1-085", "LUNG1-095", "LUNG1-194", "LUNG1-128"]
+    for i in disq_patients:  # Removing disqualified patients from clinical df
+        lung1_clinical = lung1_clinical.drop(lung1_clinical[lung1_clinical.PatientID == i].index[0])
 
-
-
+    lung1_firstorder = pd.merge(lung1_firstorder, lung1_clinical, on="PatientID", how="outer")
+    lung1_shape = pd.merge(lung1_shape, lung1_clinical, on="PatientID", how="outer")
+    lung1_glrlm = pd.merge(lung1_glrlm, lung1_clinical, on="PatientID", how="outer")
+    lung1_hlh = pd.merge(lung1_hlh, lung1_clinical, on="PatientID", how="outer")
 
 
